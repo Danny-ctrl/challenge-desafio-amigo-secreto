ac909957d0ced801727069071004d58bfa3a3699
Daniele Inspiron <contatodanny@hotmail.com>
2025-03-05 15:29:06 -0300

    botao adicionar

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let nomeAmigos = [];
+
+function adicionarNomeAmigo(){
+  let adicionar = document.getElementById("amigo");
+  let nome = adicionar.value.trim();
+  
+
+  if(nome == ""){
+    alert("Por favor, insira um nome.");
+    return;
+  
+  }
+  if(nomeAmigos.includes(nome)){
+    alert("Este nome já foi adicionado");
+    return;
+  }
+  nomeAmigos.push(nome);
+  console.log(nomeAmigos);
+  limparCampo();
+  exibirListaAmigos();
+}
+
+function limparCampo(){
+let limpar = document.getElementById("amigo");
+limpar.value="";
+}
+
+function exibirListaAmigos(){
+let lista = document.getElementById('listaAmigos');
+lista.innerHTML="";
+
+nomeAmigos.forEach((nome)=>{
+  let itemNome = document.createElement("li");
+  itemNome.textContent = nome;
+  lista.appendChild(itemNome);
+});
+}
